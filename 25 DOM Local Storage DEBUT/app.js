d8bd8c619046894ef20280172061cfe437c9efcf
Tuaks <60787667+Tuaks@users.noreply.github.com>
2020-03-03 16:53:34 +0100

    Add files via upload

---
+++ 25 DOM Local Storage DEBUT/app.js
@@ -0,0 +1,21 @@
+// DOM - Document Object Mode
+
+// Créer l'évènement save()
+/*
+
+function save() {
+    let tache = document.getElementById('tache').nodeValue;
+    localStorage.setItem('nameTask', tache)
+}
+
+// Créer l'évènement remove()
+
+function renove() {
+    document.getElementById('tache'), value= '';
+    localStorage.removeItem('nameTask')
+    
+}
+*/
+
+const 
+
